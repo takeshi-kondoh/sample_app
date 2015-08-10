51c794ec0c963e002b7e8a95941aff9ef88a4a2b
takeshi-kondoh <takeshi-kondoh@bm-sms.co.jp>
2015-08-10 17:34:06 +0900

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
+## 最新の1.9.0だと*.coffee関連でメソッド、オブジェクトエラーになる
+gem 'coffee-script-source', '1.8.0'
+
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- spec/#spec_helper - コピー.rb
+++ spec/#spec_helper - コピー.rb
@@ -3,9 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/rails'
-require 'capybara/rspec'
-
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -42,8 +39,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-  #Capybara‚Ìvisit‚ªundefined‚ÆŒ¾‚í‚ê‚½‚Æ‚«ACapybara::DSL‚ğŠÜ‚ß‚é‚æ‚¤‚É‚µ‚½‚ç‚¤‚Ü‚­‚¢‚Á‚½
-  config.include Capybara::DSL
-
 end
